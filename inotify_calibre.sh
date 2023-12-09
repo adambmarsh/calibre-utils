3926b38f09f2a6a8800e11b7a1292f8a3b46f8e6
Adam Bukolt <abukolt@zoho.com>
2023-12-09 20:10:31 +0000

    Make inotify script POSIX complieant implementng feedback from ShellCheck.
    Done, except for paths, which use env var HOME.

--- inotify_calibre.sh
+++ inotify_calibre.sh
@@ -9,16 +9,16 @@ python_pkg="$HOME/scripts/calibre-utils/main.py"
 python_log="$HOME/Downloads/books/processed/inotify.log"
 default_incoming_books_dir="$HOME/Downloads/books/in-books"
 
-if [[ ! -z "$1" ]]; then
+if [ -n "$1" ]; then
     target_dir="$1"
 else
-    target_dir=$default_incoming_books_dir
+    target_dir="$default_incoming_books_dir"
 fi
 
 # inotifywait -m -e close_write $target_dir |
-inotifywait -m -e moved_to $target_dir |
-    while read dir op file;
+inotifywait -m -e moved_to "$target_dir" |
+    while read -r dir op file;
     do         
-        # echo "dir=$dir op=$op file=$file"
-        source $activate_path && python $python_pkg -d $dir -f """$file""" > $python_log && deactivate
+        echo "dir=$dir op=$op file=$file"
+        source "$activate_path" && python "$python_pkg" -d "$dir" -f """$file""" > "$python_log" && deactivate
     done &
